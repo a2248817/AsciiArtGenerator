b326337271e01a6a420ee80bf538b49d526ac5fe
士峰 <a2248817@gmail.com>
2021-04-25 19:06:09 +0800

    test

--- AsciiArt/obj/Release/net5.0-windows/MainWindow.g.i.cs
+++ AsciiArt/obj/Release/net5.0-windows/MainWindow.g.i.cs
@@ -142,7 +142,7 @@ namespace AsciiArt {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AsciiArt;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AsciiArt;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
